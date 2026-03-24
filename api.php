fa38aeebccf158702eb818eac4b981e7b9cc08a7
Aaryesh Karle <aaryesh.k@somaiya.edu>
2026-03-25 03:08:33 +0530

    Fixed path

--- api.php
+++ api.php
@@ -15,7 +15,7 @@
  */
 
 session_start();
-require_once __DIR__ . '/php/db.php';
+require_once __DIR__ . '/db.php';
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
